a0d5c05794dd441cf5ab87c38ac0c2c3c1a6afe9
nikita <nikita@MacBook-Pro-nikita.local>
2021-02-21 20:31:21 +0200

    HW

---
+++ 2 task/js/index.js
@@ -0,0 +1,33 @@
+let countries = ['Австрия', 'Бельгия', 'Болгария', 'Великобритания', 'Венгрия', 'Германия', 'Греция', 'Дания', 'Ирландия', 'Испания', 'Италия', 'Кипр', 'Латвия', 'Литва', 'Люксембург', 'Мальта', 'Нидерланды', 'Польша', 'Португалия', 'Румыния', 'Словакия', 'Словения', 'Финляндия', 'Франция', 'Хорватия', 'Чехия', 'Швеция', 'Эстония'];
+
+function ucFirst(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+function checkValidation () {
+    let input = document.getElementById('country');
+    let message = document.getElementById('message');
+
+    if (input.value === '') {
+        message.innerText = 'Пожалуйста, заполните поле. В поле должно быть минимум 4 символа';
+        message.style.cssText = 'color: #8B0000; font-size:24px';
+    } else if (Number(input.value)) {
+        message.innerText = 'Вы ввели цифры';
+        message.style.cssText = 'color: orange; font-size:18px';
+    } else if (input.value.length <= 4) {
+        message.innerText = 'Вы ввели меньше 4 букв';
+        message.style.cssText = 'color: red; font-size:28px';
+    } else {
+        let finalMessage = 'Такой страны нет в Евросоюзе';
+        let styleMessageText = 'color: #B22222; font-size:28px';
+        let ucFirstValue = ucFirst(input.value);
+        countries.forEach( function (name) {
+            if ( ucFirstValue === name ) {
+                finalMessage = ucFirstValue + ' есть в Евросоюзе';
+                styleMessageText = 'color: green; font-size:32px';
+            }
+        })
+        message.innerText = finalMessage;
+        message.style.cssText = styleMessageText;
+    }
+}
