0178a41550910eaa783407e8e02766a4f1b6320d
nikita <nikita@MacBook-Pro-nikita.local>
2021-02-21 23:28:44 +0200

    Fix 2 task

--- 2 task/js/index.js
+++ 2 task/js/index.js
@@ -8,26 +8,27 @@ function checkValidation () {
     let input = document.getElementById('country');
     let message = document.getElementById('message');
 
-    if (input.value === '') {
-        message.innerText = 'Пожалуйста, заполните поле. В поле должно быть минимум 4 символа';
-        message.style.cssText = 'color: #8B0000; font-size:24px';
-    } else if (Number(input.value)) {
-        message.innerText = 'Вы ввели цифры';
-        message.style.cssText = 'color: orange; font-size:18px';
-    } else if (input.value.length <= 4) {
-        message.innerText = 'Вы ввели меньше 4 букв';
-        message.style.cssText = 'color: red; font-size:28px';
-    } else {
-        let finalMessage = 'Такой страны нет в Евросоюзе';
-        let styleMessageText = 'color: #B22222; font-size:28px';
-        let ucFirstValue = ucFirst(input.value);
-        countries.forEach( function (name) {
-            if ( ucFirstValue === name ) {
-                finalMessage = ucFirstValue + ' есть в Евросоюзе';
-                styleMessageText = 'color: green; font-size:32px';
-            }
-        })
-        message.innerText = finalMessage;
-        message.style.cssText = styleMessageText;
+    switch (true) {
+        case input.value === '':
+            message.innerText = 'Пожалуйста, заполните поле. В поле должно быть минимум 4 символа';
+            message.style.cssText = 'color: #8B0000; font-size:24px';
+        case (Number(input.value)):
+            message.innerText = 'Вы ввели цифры';
+            message.style.cssText = 'color: orange; font-size:18px';
+        case (input.value.length <= 4):
+            message.innerText = 'Вы ввели меньше 4 букв';
+            message.style.cssText = 'color: red; font-size:28px';
+        default:
+            let finalMessage = 'Такой страны нет в Евросоюзе';
+            let styleMessageText = 'color: #B22222; font-size:28px';
+            let ucFirstValue = ucFirst(input.value);
+            countries.forEach( (name) => {
+                if ( ucFirstValue === name ) {
+                    finalMessage = ucFirstValue + ' есть в Евросоюзе';
+                    styleMessageText = 'color: green; font-size:32px'
+                }
+            })
+            message.innerText = finalMessage;
+            message.style.cssText = styleMessageText;
     }
 }
